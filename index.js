ce4e8bbbe04463a7a0b69fbc2dc4c0872eae9f75
Marion Nabulobi <arinabulobi@gmail.com>
2024-10-10 00:00:12 +0300

    add first class functions

--- index.js
+++ index.js
@@ -1,1 +1,18 @@
-// Code your solution in this file!
+let drivers = ['Antonia', 'Nuru', 'Amari', 'Mo'];
+
+function returnFirstTwoDrivers(drivers){
+    return drivers.slice(0,2);
+}
+console.log('returnFirstTwoDrivers'(drivers));
+
+function returnLastTwoDrivers(drivers){
+        return drivers.slice(-2);
+}
+console.log('returnLastTwoDrivers'(drivers));
+
+function selectingDrivers(drivers) {
+    returnFirstTwoDrivers();
+    returnLastTwoDrivers();
+}
+console.log(selectingDrivers[0](returnFirstTwoDrivers));
+console.log(selectingDrivers[1](returnLastTwoDrivers));
